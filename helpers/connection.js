e588db45f40fc3b2972a065515671043da340052
vivek8690 <vivek.prajapati.ldce@gmail.com>
2020-08-07 19:51:40 +0530

    docker configuration removed

--- helpers/connection.js
+++ helpers/connection.js
@@ -3,11 +3,9 @@
 /* eslint-disable no-undef */
 const mongoose = require('mongoose');
 
-console.log('process.env.NODE_ENV:', process.env.NODE_ENV);
-
 const DB_URL = process.env.DATABASE_URL || 'mongodb://localhost:27017,localhost:27018,localhost:27019/banking';
 
-mongoose.connect(DB_URL, { replicaSet: 'rs' });
+mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true, replicaSet: 'rs' });
 
 const db = mongoose.connection;
 
