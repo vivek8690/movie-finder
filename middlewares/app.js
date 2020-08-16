6b09f3d5e488a4af1f59a3662f25adc6d7af45a8
vivek8690 <vivek.prajapati.ldce@gmail.com>
2020-08-16 19:10:43 +0530

    static files added

--- middlewares/app.js
+++ middlewares/app.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const movieRouter = require('../routes/movie');
 const genreRouter = require('../routes/genre');
@@ -22,6 +23,11 @@ app.use((req, res, next) => {
 if (process.env.NODE_ENV !== 'test') {
     app.use(logger('dev'));
 }
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
---
+++ middlewares/public/precache-manifest.a3c04700b785a9b4c35ea862329682df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf379624fc4b36e6ec2c3e3beb2e5af8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "676b7b5b33f528d045a2",
+    "url": "/static/css/2.dc646829.chunk.css"
+  },
+  {
+    "revision": "0c7b40b9c5ad774c4026",
+    "url": "/static/css/main.df19ff5e.chunk.css"
+  },
+  {
+    "revision": "676b7b5b33f528d045a2",
+    "url": "/static/js/2.5e290c57.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.5e290c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7b40b9c5ad774c4026",
+    "url": "/static/js/main.6a74d09d.chunk.js"
+  },
+  {
+    "revision": "c4c6b87194d0f9f9f47e",
+    "url": "/static/js/runtime-main.6d734b6a.js"
+  }
+]);
